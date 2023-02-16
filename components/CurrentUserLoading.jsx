86d6dd5cce59f0a0c13481e5c92851d5edb4b381
r-bytes / <101632683+r-bytes@users.noreply.github.com>
2023-02-16 15:37:06 +0100

    refactor: cleanup

--- components/CurrentUserLoading.jsx
+++ components/CurrentUserLoading.jsx
@@ -5,21 +5,33 @@ import React, { useState, useEffect } from 'react'
 const CurrentUserLoading = ({ children }) => {
     const [user, setUser] = useState(null);
     const [products, setProducts] = useState(null);
+    const [allUsers, setAllUsers] = useState(null);
+    
     useEffect(() => {
-        // (async () => {
-        //     const response = await axios.get("/api/current-user")
-        //     setUser(response.data)
-        // })()
+        (async () => {
+            const response = await axios.get("/api/current-user")
+            setUser(response.data)
+        })()
+    }, []);
 
+    useEffect(() => {
         (async () => {
             const response = await axios.get("/api/products")
             setProducts(response.data)
         })()
+    }, []);
 
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get("/api/users")
+            setAllUsers(response.data)
+        })()
     }, []);
 
 
+    console.log("currentUser", user)
     console.log("products", products)
+    console.log("allUsers", allUsers)
     
     // return (
     //     <div>
